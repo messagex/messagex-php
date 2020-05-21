83eba40db5a6277522d577d4a251292d05747847
Damien <damien.buttler@smsglobal.com>
2020-05-21 12:18:12 +1000

    Refactored test server to a class.

--- tests/router.php
+++ tests/router.php
@@ -1,44 +1,69 @@
 <?php
 
-$method = $_SERVER['REQUEST_METHOD'];
-$uri = $_SERVER['REQUEST_URI'];
-$protocol = $_SERVER['SERVER_PROTOCOL'];
+$r = new Router();
+$r->processRequest();
 
-$data = parseInput();
-logger("====================");
-logger($data);
-logger("====================");
-$request = $method .' '. $uri;
-
-$requestMap = [
-    'POST /authorise' => 'authorise.php',
-];
-
-if (array_key_exists($request, $requestMap)) {
-    logger("------------- request made: $request");
-    include(__DIR__ .'/Factories/'. $requestMap[$request]);
-
-    sendResponse($response);
-}
-
-sendResponse($data);
-
-function sendResponse($response)
+class Router
 {
-    http_response_code($response['statusCode']);
-    header('Content-Type', 'application/json');
-    echo $response['body'];
-    exit;
-}
+    private $method;
+    private $uri;
+    private $protocol;
+    private $data;
+    private $requestIndex;
 
-function parseInput()
-{
-    $data = file_get_contents('php://input');
 
-    return json_decode($data, true);
-}
+    public function __construct()
+    {
+        $this->logger('--------- constructing');
+        $this->method = $_SERVER['REQUEST_METHOD'];
+        $this->uri = $_SERVER['REQUEST_URI'];
+        $this->protocol = $_SERVER['SERVER_PROTOCOL'];
 
-function logger($msg)
-{
-    file_put_contents('/tmp/phpd.log', date('Y-m-d H:i:s') .' - '. print_r($msg, true) ."\n", FILE_APPEND);
+        $this->requestIndex = $this->method .' '. $this->uri;
+
+        $this->data = $this->parseInput();
+    }
+
+    private function getRequestMap()
+    {
+        return [
+            'POST /authorise' => 'authorise.php',
+        ];
+    }
+
+    public function processRequest()
+    {
+        if (array_key_exists($this->requestIndex, $this->getRequestMap()))
+        {
+            $this->logger("------------- request made: ". $this->requestIndex);
+            include (__DIR__ .'/Factories/'. $this->getRequestMap()[$this->requestIndex]);
+
+            $this->sendResponse($response);
+        }
+
+        $this->sendResponse($this->data);
+    }
+
+
+
+
+    private function sendResponse($response)
+    {
+        http_response_code($response['statusCode']);
+        header('Content-Type', 'application/json');
+        echo $response['body'];
+        exit;
+    }
+
+    private function parseInput()
+    {
+        $data = file_get_contents('php://input');
+
+        return json_decode($data, true);
+    }
+
+    private function logger($msg)
+    {
+        file_put_contents('/tmp/phpd.log', date('Y-m-d H:i:s') . ' - ' . print_r($msg, true) . "\n", FILE_APPEND);
+    }
 }
